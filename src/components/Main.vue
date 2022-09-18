1604d2e17968b7efb1adde40edc1f3ce776f9da4
Jorge Longoria <jlongo48@stu.southtexascollege.edu>
2022-09-17 20:58:16 -0500

    Added cards with pictures and descriptions

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,17 +1,17 @@
 <script setup>
   import MainSearch from './MainSearch.vue'
+  import MainCards from './MainCards.vue'
 </script>
 
 <template>
   <div class="wrapper">
     <MainSearch />
-    <h1>Hello</h1>
+    <MainCards />
   </div>
 </template>
 
 <style scoped lang="postcss">
-    .wrapper {
-        @apply container mx-auto flex flex-col gap-4 py-4;
-    }
-
+  .wrapper {
+    @apply container mx-auto flex flex-col gap-4 py-4;
+  }
 </style>
---
+++ src/components/MainCardsSingle.vue
@@ -0,0 +1,47 @@
+<script setup>
+  import { faker } from '@faker-js/faker'
+
+  const firstName = faker.name.firstName()
+  const lastName = faker.name.lastName()
+  const fullName = `${firstName} ${lastName}`
+
+  const selectCard = () => {
+    console.log(`${fullName} selected`)
+  }
+</script>
+
+<template>
+  <div class="card" @click="selectCard">
+    <div class="card-image">
+      <img :src="faker.internet.avatar()" alt="" srcset="" />
+    </div>
+    <div class="card-details">
+      <p class="card-details-name">{{ fullName }}</p>
+      <p class="card-details-job">{{ faker.name.jobTitle() }}, {{ faker.name.jobArea() }}</p>
+      <p class="card-details-quote">"{{ faker.lorem.paragraph() }}"</p>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+  .card {
+    @apply cursor-pointer overflow-hidden rounded-md bg-gray-300 p-6 shadow-md transition-transform duration-300 hover:scale-110 hover:shadow-2xl hover:shadow-gray-900;
+    &-image {
+      img {
+        @apply mx-auto rounded-full object-contain;
+      }
+    }
+    &-details {
+      @apply flex flex-col gap-2  pt-6 text-center;
+      &-name {
+        @apply text-3xl font-thin  tracking-wider text-gray-800;
+      }
+      &-job {
+        @apply -mt-2 text-xs font-bold text-red-600;
+      }
+      &-quote {
+        @apply pt-4 text-lg italic text-gray-800;
+      }
+    }
+  }
+</style>
